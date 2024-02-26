349bde0fda985fd00c69fe5bc345cd339db33310
Bobur Yusupov <bobur.yu@anur.tech>
2024-02-26 18:44:27 +0500

    Completed finding_biggest_sale.py

--- pandas/finding_biggest_sale.py
+++ pandas/finding_biggest_sale.py
@@ -1,21 +1,24 @@
-import pandas
+import pandas as pd
 
-df = pandas.read_csv('vgsales.csv')
+df = pd.read_csv('vgsales.csv')
 
 df = df.dropna(axis=0)
+
 dict_years = {}
 
-years = list(df['Year'])
-sales = list(df['Global_Sales'])
-
-
-for year in years:
-    inx = years.index(year)
-    if year in dict_years.keys():
-        dict_years[year] += sales[inx]
+for index, row in df.iterrows():
+    year = row['Year']
+    sales = row['Global_Sales']
+    
+    if year in dict_years:
+        dict_years[year] += float(sales)
 
     else:
-        dict_years[year] = sales[inx]
+        dict_years[year] = float(sales)
 
+max_sales = max(dict_years.values())
 
-print(dict_years)
+years_with_max_sales = [year for year, sales in dict_years.items() if sales == max_sales]
+
+print("Year(s) with the greatest global sales:", years_with_max_sales)
+print("Greatest global sales:", max_sales)
